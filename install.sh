8d6b43e296131d2b191f98863f5011fafc281105
ehsan shariati <ehsan6sha@gmail.com>
2025-02-17 00:00:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -93,7 +93,7 @@ Group=nodejs
 WorkingDirectory=${APP_DIR}
 Environment=NODE_ENV=production
 Environment=PORT=3000
-ExecStart=/usr/bin/node .next/standalone/server.js
+ExecStart=/usr/bin/node ${APP_DIR}/.next/standalone/server.js
 EnvironmentFile=${APP_DIR}/.env.local
 Restart=on-failure
 RestartSec=120
